d334aec34dd15e3bac119e6e3f0f28812c40e8d3
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-11-30 17:36:15 -0300

    Update postgresSQL.py

--- bd/postgresSQL.py
+++ bd/postgresSQL.py
@@ -1,38 +1,38 @@
+import psycopg2
 """
 Importação:
 
-Importa a biblioteca do PostgreSQL.
-"""
-import psycopg2
-
-
-"""
-Classe PostgressDB:
-
-Gerenciar as operações do banco de dados, como conectar, inserir e desconectar.
+1. Importa a biblioteca do PostgreSQL.
 """
 
 
 class PostgressDB:
-
     """
-    Construção da classe:
+    Classe PostgressDB:
 
-    Atributos (privados):
-
-    Inicializa a classe com os parâmetros de conexão do banco de dados, com dados pré definidos caso não seja informado:
-    dbname (str): nome do banco (padrão: 'mydatabase');
-    user (str): nome do usuário (padrão: 'user');
-    password (str): senha do usuário (padrão: 'password');
-    host (str): endereço do banco (padrão: 'localhost);
-    port (str): porta do banco (padrão: '5410);
-
-    conn: atributo que contém a conexão do banco (inicializado com None);
-    cursor: atributo que contém o cursor do banco (inicializado com None);
-    connect: a conexão com o banco de dados é estabelecida.
+    Gerenciar as operações do banco de dados Postgress.
+    Permite conectar, inserir mensagem e desconectar.
     """
 
     def __init__(self, dbname: str = 'mydatabase', user: str = 'user', password: str = 'password', host: str = 'localhost', port: str = '5410') -> None:
+        """
+        Construção da classe:
+
+        Inicializa a instância do PostgressDB e estabelece uma conexão com o banco.
+
+        -> Atributos (privados):
+
+        Inicializa a classe com os parâmetros de conexão do banco de dados, com dados pré definidos caso não seja informado:
+        dbname (str): nome do banco (padrão: 'mydatabase');
+        user (str): nome do usuário (padrão: 'user');
+        password (str): senha do usuário (padrão: 'password');
+        host (str): endereço do banco (padrão: 'localhost);
+        port (str): porta do banco (padrão: '5410);
+
+        conn: atributo que contém a conexão do banco (inicializado com None);
+        cursor: atributo que contém o cursor do banco (inicializado com None);
+        connect: a conexão com o banco de dados é estabelecida.
+        """
         self._dbname = dbname
         self._user = user
         self._password = password
@@ -42,16 +42,15 @@ class PostgressDB:
         self._cursor = None
         self.connect()
 
-    """
-    Método connect:
-
-    Verifica de a variável conn é None.
-    Se sim, estabelece conexão com o banco de dados usando 'psycopg2.connect'.
-    Inicializa o cursor para executar os comandos SQL.
-    Chama o método 'create_table' para criar a tabela caso não exista.
-    """
-
     def connect(self) -> None:
+        """
+        Método connect:
+
+        Verifica de a variável conn é None.
+        Se sim, estabelece conexão com o banco de dados usando 'psycopg2.connect'.
+        Inicializa o cursor para executar os comandos SQL.
+        Chama o método 'create_table' para criar a tabela caso não exista.
+        """
         if (self._conn == None):
             self._conn = psycopg2.connect(
                 dbname=self._dbname,
@@ -64,14 +63,13 @@ class PostgressDB:
             self._cursor = self._conn.cursor()
             self.create_table()
 
-    """
-    Método create_table:
-
-    Cria a tabela com as colunas: id, role, message e date.
-    O comando 'self._conn.commit()' é usado para salvar as alterações.
-    """
-
     def create_table(self) -> None:
+        """
+        Método create_table:
+
+        Cria a tabela com as colunas: id, role, message e date.
+        O comando 'self._conn.commit()' é usado para salvar as alterações.
+        """
         self._cursor.execute("""
             CREATE TABLE IF NOT EXISTS messages (
                 id SERIAL PRIMARY KEY,
@@ -82,20 +80,19 @@ class PostgressDB:
         """)
         self._conn.commit()
 
-    """
-    Método insert:
-
-    Parâmetro:
-    dado (lista[tupla[str, str]]): uma lista de tuplas, na qual cada uma contém dois elementos do tipo string.
-        - O primeiro elemento corresponde ao nome de quem enviou a mensagem
-        - O segundo elemento corresponde a mensagem que foi enviada
-
-    Verifica se a lista não está vazia.
-    Se não, o comando 'executemany' insere multiplas linhas de uma vez só (as duas strings passadas em cada tupla da lista).
-    O comando 'self._conn.commit()' é usado para salvar as alterações.
-    """
-
     def insert(self, dados: list[tuple[str, str]]) -> None:
+        """
+        Método insert:
+
+        Parâmetro:
+        dado (lista[tupla[str, str]]): uma lista de tuplas, na qual cada uma contém dois elementos do tipo string.
+            - O primeiro elemento corresponde ao nome de quem enviou a mensagem
+            - O segundo elemento corresponde a mensagem que foi enviada
+
+        Verifica se a lista não está vazia.
+        Se não, o comando 'executemany' insere multiplas linhas de uma vez só (as duas strings passadas em cada tupla da lista).
+        O comando 'self._conn.commit()' é usado para salvar as alterações.
+        """
         if (dados):
             self._cursor.executemany("""
                 INSERT INTO messages (role, message)
@@ -103,37 +100,35 @@ class PostgressDB:
             """, dados)
             self._conn.commit()
 
-    """
-    Método select_all:
-
-    O comando executado 'SELECT * FROM messages' seleciona todas as linhas da tabela messages.
-    No for, é exibido todas as linhas resultante.
-    """
-
     def select_all(self) -> None:
+        """
+        Método select_all:
+
+        O comando executado 'SELECT * FROM messages' seleciona todas as linhas da tabela messages.
+        No for, é exibido todas as linhas resultante.
+        """
         self._cursor.execute("SELECT * FROM messages")
         for row in self._cursor.fetchall():  # Recupera todas as linhas resultantes do comando anterior e retorna como uma lista de tuplas
             print(row)
 
-    """
-    Método disconnect:
-
-    Fecha o cursor e a conexão (conn) com o banco de dados usando o comando close.
-    """
-
     def disconnect(self) -> None:
+        """
+        Método disconnect:
+
+        Fecha o cursor e a conexão (conn) com o banco de dados usando o comando close.
+        """
         self._cursor.close()
         self._conn.close()
 
 
-"""
-Bloco Principal:
-
-Verifica se o script está sendo executado diretamente. 
-Cria uma instância da classe PostgressDB.
-Chama o método select_all para imprimir todas as mensagens no banco de dados.
-"""
 if (__name__ == "__main__"):
+    """
+    Bloco Principal:
+
+    Verifica se o script está sendo executado diretamente. 
+    Cria uma instância da classe PostgressDB.
+    Chama o método select_all para imprimir todas as mensagens no banco de dados.
+    """
     banco = PostgressDB()
     # banco.insert([("funcionario", "mensagemOi")])
     banco.select_all()
