a2d554edb900e5fb05fb1f2c1d7d9ed32af9794f
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-11-30 17:35:56 -0300

    Update funcionario_main.py

--- chat/funcionario_main.py
+++ chat/funcionario_main.py
@@ -1,12 +1,18 @@
-""" 
-Importa todas as definições do módulo 'user_class'.
-Verifica se o código está sendo executado diretamente.
-Se sim, cria uma instância de usuário com:
-    - nome: nome_admin
-Utiliza o método iniciar do usuário
-"""
 from user_class import *
+""" 
+Importação: 
 
-if(__name__ == "__main__"):
+1. Importa todas as definições do módulo 'user_class'.
+"""
+
+if (__name__ == "__main__"):
+    """
+    Bloco Principal:
+
+    Verifica se o código está sendo executado diretamente.
+    Se sim, cria uma instância de usuário com:
+        - nome: nome_admin
+    Utiliza o método iniciar do usuário
+    """
     admin = User(nome_admin)
     admin.iniciar()
