3366175275b60c3ef65635ae6032c2d5b1736c87
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-11-30 17:36:12 -0300

    Update redis_bd.py

--- bd/redis_bd.py
+++ bd/redis_bd.py
@@ -1,48 +1,69 @@
+"""
+Importação:
+
+1. Importa a biblioteca do Redis.
+2. Importa o datetime para manipular datas e horas.
+"""
 import redis
 from datetime import datetime
 
-class RedisDB:
 
+class RedisDB:
     """
-    Classe para gerenciar a interação com um banco de dados Redis.
+    Classe RedisDB:
+
+    Gerenciar a interação com um banco de dados Redis.
     Permite criar, listar e excluir mensagens, limpar o banco e fechar a conexão.
     """
 
     def __init__(self, host: str = 'localhost', port: int = 6380, decode_responses: bool = True) -> None:
         """
-        Inicializa a instância do RedisDB e estabelece uma conexão com o Redis.
+        Construção da classe:
 
-        host (str): Endereço do servidor Redis (padrão: 'localhost').
-        port (str): Porta do servidor Redis (padrão: 6380).
-        decode_responses (str): Define se as respostas do Redis devem ser decodificadas como strings (padrão: True).
+        Inicializa a instância do RedisDB e estabelece uma conexão com o banco.
+
+        -> Atributos (privados):
+
+        Inicializa a classe com os parâmetros de conexão do banco de dados, com dados pré definidos caso não seja informado:
+        host (str): endereço do servidor Redis (padrão: 'localhost').
+        port (str): porta do servidor Redis (padrão: 6380).
+        decode_responses (str): define se as respostas do Redis devem ser decodificadas como strings (padrão: True).
+
+        client (objeto): a conexão com o Redis
+        connect: a conexão com o banco de dados é estabelecida.
         """
         self._host = host
         self._port = port
         self._decode_responses = decode_responses
-        self._client = None 
+        self._client = None
         self.connect()
-
 
     def connect(self) -> None:
         """
-        Estabelece uma conexão com o servidor Redis.
+        Método connect:
+
+        Verifica de a variável conn é None.
+        Se sim, cria uma nova instância do cliente Redis.
         """
-        if(self._client is None):
+        if (self._client is None):
             self._client = redis.Redis(
                 host=self._host,
                 port=self._port,
                 decode_responses=self._decode_responses
             )
 
-
     def create_message(self, role: str, message: str) -> None:
         """
-        Cria e armazena uma mensagem no Redis.
+        Método create_message:
 
-        role (str): Papel associado à mensagem (ex.: 'usuario', 'funcionario').
-        message (str): Conteúdo da mensagem.
+        -> Parâmetros:
+        role (str): papel associado à mensagem.
+        message (str): conteúdo da mensagem.
+
+        Gera um timestamp atual no formato ISO 8601.
+        Cria uma chave única para a mensagem.
+        Armazena dados na forma de um hash (estrutura de dados similar a um dicionário) em Redis.
         """
-        
         timestamp = datetime.now().isoformat()
         key = f"message:{role}:{timestamp}"
         self._client.hset(key, mapping={
@@ -50,20 +71,27 @@ class RedisDB:
             'message': message,
             'timestamp': timestamp
         })
-    
 
-    def list_messages(self) -> list[dict[str : str]]:
+    def list_messages(self) -> list[dict[str: str]]:
         """
-        Lista todas as mensagens armazenadas no Redis.
+        Método list_messages:
 
-        Retorno: Lista de dicionários contendo as mensagens.
+        -> Variáveis:
+        messages (lista): armazena todas as mensagens recuperadas do Redis.
+        cursor (int): mantem o estado da iteração.
+        pattern (str): define quais chaves o comando SCAN deve procurar.
+
+        Procura chaves de 10 em 10 que correspondem ao pattern
+        Adiciona na lista todos os valores armazenados no hash daquela chave.
+        Retorna a lista com as mensagens encontradas.
         """
         messages = []
         cursor = 0
         pattern = "message:*"
 
         while True:
-            cursor, keys = self._client.scan(cursor=cursor, match=pattern, count=10)
+            cursor, keys = self._client.scan(
+                cursor=cursor, match=pattern, count=10)
             for key in keys:
                 messages.append(self._client.hgetall(key))
                 print(messages[-1])
@@ -71,35 +99,42 @@ class RedisDB:
                 break
 
         return messages
-    
 
     def delete_message(self, key: str) -> None:
         """
-        Exclui uma mensagem específica do Redis.
+        Método delete_message:
 
-        key (str): A chave da mensagem a ser excluída.
+        -> Parâmetro:
+        key (str): a chave da mensagem a ser excluída.
+
+        Exclui uma mensagem mandando por parâmetro do Redis.
         """
         self._client.delete(key)
 
-
     def clear_all_data(self) -> None:
         """
+        Método clear_all_data:
+
         Remove todos os dados armazenados no Redis.
         """
         self._client.flushdb()
 
-
     def close(self) -> None:
         """
+        Método close:
+
         Fecha a conexão com o servidor Redis.
         """
         self._client.close()
 
 
-if(__name__ == "__main__"):
+if (__name__ == "__main__"):
     """
-    Teste de funcionalidades. 
-    Exibe todas as mensagens do banco.
+    Bloco Principal:
+
+    Cria uma instância da classe RedisDB
+    Retorna todas as mensagens armazenadas do Redis
+    Chama o método close para desconectar do banco de dados.
     """
     redisDb = RedisDB()
 
