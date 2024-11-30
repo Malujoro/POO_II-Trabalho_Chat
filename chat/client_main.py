9a0dc0f7c90d6287155d44b239a8c2f92a1067d1
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-11-30 17:35:58 -0300

    Update client_main.py

--- chat/client_main.py
+++ chat/client_main.py
@@ -1,13 +1,19 @@
-""" 
-Importa todas as definições do módulo 'user_class'.
-Verifica se o código está sendo executado diretamente.
-Se sim, cria uma instância de usuário com:
-    - nome: nome_cliente
-    - timeout: 300
-Utiliza o método iniciar do usuário
-"""
 from user_class import *
+""" 
+Importação:
 
-if(__name__ == "__main__"):
+1. Importa todas as definições do módulo 'user_class'.
+"""
+
+if (__name__ == "__main__"):
+    """
+    Bloco Principal:
+
+    Verifica se o código está sendo executado diretamente.
+    Se sim, cria uma instância de usuário com:
+        - nome: nome_cliente
+        - timeout: 300
+    Chama o método iniciar do cliente.
+    """
     cliente = User(nome_cliente, 300)
     cliente.iniciar()
