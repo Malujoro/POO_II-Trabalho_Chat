c43471754eefd27fd8e0eb133408cf1892934794
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-11-30 17:35:41 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,6 @@
+from bd.redis_bd import *
+from random import randint
+from chat.variaveis import *
 """
 Importações:
 
@@ -5,27 +8,24 @@ Importações:
 2. Incluir a função randint para criar números aleatórios.
 3. Importa as variáveis 'nome_cliente' e 'nome_admin'.
 """
-from bd.redis_bd import *
-from random import randint
-from chat.variaveis import *
 
-"""
-Bloco principal:
-
-Verifica se o script está sendo executado diretamente. 
-Se sim, cria-se uma lista nomeada 'usuarios' contendo 'nome_cliente' e 'nome_admin' (são os nomes dos usuários).
-Cria uma instância do Redis que será usada para interagir com o banco de dados.
-"""
 if __name__ == "__main__":
+    """
+    Bloco principal:
+
+    Verifica se o script está sendo executado diretamente. 
+    Se sim, cria-se uma lista nomeada 'usuarios' contendo 'nome_cliente' e 'nome_admin' (são os nomes dos usuários).
+    Cria uma instância do Redis que será usada para interagir com o banco de dados.
+    """
     usuarios = [nome_cliente, nome_admin]
     redisDB = RedisDB()
 
-    """
-    For:
-
-    Inicia um loop de 1 milhão de vezes.
-    Seleciona um usuário aleatório da lista de usuários, sendo 0 para cliente e 1 para funcionário.
-    Chama a função do arquivo redisDB.py.
-    """
     for i in range(1000000):
+        """
+        For:
+
+        Inicia um loop de 1 milhão de vezes.
+        Seleciona um usuário aleatório da lista de usuários, sendo 0 para cliente e 1 para funcionário.
+        Chama a função do arquivo redisDB.py.
+        """
         redisDB.create_message(usuarios[randint(0, 1)], f"mensagem{i+1}")
