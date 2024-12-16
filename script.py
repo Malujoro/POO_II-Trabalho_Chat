58cad83b8749d163274f8729b0cf79068e837d97
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-12-16 15:44:39 -0300

    Update script.py

--- script.py
+++ script.py
@@ -6,17 +6,37 @@ from multiprocessing.pool import ThreadPool
 """
 Importações:
 
-1. Incluir classes e funções para lidar com o banco de dados Redis.
-2. Incluir a função randint para criar números aleatórios.
+1. Importa o módulo worker para interagir com o banco de dados Redis.
+2. Importa a função randint para criar números aleatórios.
 3. Importa as variáveis 'nome_cliente' e 'nome_admin'.
+4. Importa a classe ThreadPool para executar as tarefas com Threads.
 """
 
+
 def generate_data(i):
+    """
+    Método generate_data:
+
+    -> Parâmetro:
+    i (int): índice.
+
+    Gera uma lista com o tipo do cliente - que será gerado aleatoriamente - e a mensagem.
+    Gera uma mensagem padrão baseada no índice.
+    Retorna a lista.
+    """
     usuarios = [nome_cliente, nome_admin]
     return usuarios[randint(0, 1)], f"mensagem{i+1}"
 
 
 def sender(data: tuple[str, str]):
+    """
+    Método sender:
+
+    -> Parâmetro:
+    data (tupla[str]): informações que serão salvos no Redis.
+
+    Envia uma mensagem pro Redis com a tupla.
+    """
     redisDB.create_message(data[0], data[1])
 
 
@@ -25,8 +45,11 @@ if __name__ == "__main__":
     Bloco principal:
 
     Verifica se o script está sendo executado diretamente. 
-    Se sim, cria-se uma lista nomeada 'usuarios' contendo 'nome_cliente' e 'nome_admin' (são os nomes dos usuários).
-    Cria uma instância do Redis que será usada para interagir com o banco de dados.
+    Se sim, cria-se uma instância do Redis que será usada para interagir com o banco de dados.
+    Define o número de threads.
+    Define o total de registros a serem criados.
+    Define o tamanho de cada pacote.
+    Calcula a quantidade de pacotes necessários.
     """
     redisDB = RedisDB()
     num_threads = 4
@@ -37,7 +60,6 @@ if __name__ == "__main__":
 
     # with ThreadPool(num_threads) as pool:
     #     batch_results = pool.map(generate_data, range(total_records))
-
 
     # for i in range(total_records):
     #     """
@@ -53,16 +75,23 @@ if __name__ == "__main__":
     num_batches = (total_records + batch_size - 1) // batch_size
 
     for batch_num in range(num_batches):
+        """
+        For:
+
+        Para cada pacote, calcula o seu tamnho.
+        Utiliza threads para gerar dados com o método 'generate_data' e os armazena na 'list_objects'.
+        Utiliza threads para enviar os dados gerados no 'sender' para o Redis.        
+        """
         print(f"Batch {batch_num + 1}/{num_batches}")
 
-        current_batch_size = min(batch_size, total_records - batch_num * batch_size)
+        current_batch_size = min(
+            batch_size, total_records - batch_num * batch_size)
 
         with ThreadPool(num_threads) as pool:
             list_objects = pool.map(generate_data, range(current_batch_size))
 
         print(f"{len(list_objects)} mensagens no batch {batch_num + 1}")
 
-        # Enviar dados do lote para o Redis
         with ThreadPool(num_threads) as pool:
             pool.map(sender, list_objects)
 
