71126d2e29f358808b4b873b95c47b7af8581b57
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-12-16 15:44:43 -0300

    Update __init__.py

--- worker/src/worker/__init__.py
+++ worker/src/worker/__init__.py
@@ -1,3 +1,11 @@
 from worker.worker import Worker
 from worker.postgresSQL import PostgressDB
 from worker.redis_bd import RedisDB
+
+"""
+Importações:
+
+1. Importa a classe Worker
+2. Importa a classe PostgressDB
+3. Importa a classe RedisDB
+"""
